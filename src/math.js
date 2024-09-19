9adacf2151d8adf1428c15308776d5e58a63ebe7
JUSTIN SCOTT <JUSTINSCOTT820@GMAIL.COM>
2024-09-18 17:25:18 -0700

    demo app

---
+++ src/math.js
@@ -0,0 +1,10 @@
+var foo = 30; // does not create window.foo
+
+const sum = (a, b) => a + b;
+const numbers = [5, 10, 15, 20, 25, foo];
+
+export { sum, numbers};
+
+const product = (a, b) => a * b;
+
+export default product;
